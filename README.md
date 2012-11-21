a7dfc014c99a91a2ffce3108c664922d47d5cccf
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2012-11-21 09:58:06 -0500

    Initial commmit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe
+
+Simple Rails application. No database, no user, no scores, no frills.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToe::Application.config.secret_token = '24e8e32aea47edc34c61b164609fef42e05625d6ecd8170b3e32fd61425778e955916b90199523e6800ee9920c0088d18fa292b8333b476b3573bbb6db47f013'
