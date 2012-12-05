e219f88ae49b91eb82f9587476477a594a3822e3
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2012-12-05 11:51:40 -0500

    Fix Heroku mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address              => 'smtp-server.tampabay.rr.com',
-  :port                 => '587'
+  :port                 => '587',
   :enable_starttls_auto => true
 }
 
