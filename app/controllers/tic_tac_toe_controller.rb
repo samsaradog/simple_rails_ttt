263585eea503b53910bc908b65ae5094e4db64f1
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2012-12-19 09:27:41 -0500

    Refactor logic into game state model

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -23,6 +23,51 @@ class TicTacToeController < ApplicationController
     create_return(current_game)
   end
   
+  # Above is for the computer vs. human game
+  # Below is for the two-human game
+  
+  def initialize_game
+    current_record = GameState.create_game
+    redirect_to "/#{current_record.cipher(X_TOKEN)}"
+  end
+  
+  def two_player_game
+    current_record = GameState.find_by_cipher(params[:cipher].to_i)
+    
+    @player         = Cipher.decode_player(params[:cipher].to_i)
+    @home_button    = X_TOKEN == @player
+    @notification   = create_notification(current_record)
+    @representation = current_record.game.representation
+  end
+  
+  def reset_two_player_game
+    current_record = GameState.find_by_cipher(params[:cipher].to_i)
+    current_record.reset
+    current_record.save
+    create_player_return(current_record)
+  end
+  
+  def two_player_move
+    puts params.inspect
+    current_record = GameState.find_by_cipher(params[:cipher].to_i)
+    current_record.move_if_possible!(params[:move])
+    create_player_return(current_record)
+  end
+  
+  def get_update
+    current_record = GameState.find_by_cipher(params[:cipher].to_i)
+    create_player_return(current_record)
+  end
+  
+  def invite
+    new_cipher = Cipher.switch_cipher_player(params[:cipher].to_i)
+    Invite.tic_tac_toe(params[:user_email],
+    "#{request.protocol}#{request.host_with_port}\/#{new_cipher}").deliver
+    redirect_to :back
+  end
+  
+  private
+  
   def set_cookie_value(cookie_value)
     cookies.signed[:game_state] = cookie_value
   end
@@ -35,78 +80,16 @@ class TicTacToeController < ApplicationController
     render json: { notification:   game.notification,
                    representation: game.representation }.to_json
   end
-  
-  # Above is for the computer vs. human game
-  # Below is for the two-human game
-  
-  def initialize_game
-    current_record = GameState.new
-    current_record.initialize_player
-    current_record.reset
-    current_record.save
     
-    redirect_to "/#{current_record.cipher(X_TOKEN)}"
+  def create_player_return(record)
+    render json: { notification: create_notification(record),
+                   representation: record.game.representation }.to_json
   end
   
-  def two_player_game
-    current_record = fetch_record(params[:cipher].to_i)
-    current_game = Game.new(current_record.token)
-    
-    @player         = Cipher.decode_player(params[:cipher].to_i)
-    @home_button    = X_TOKEN == @player
-    @notification   = create_notification(current_game,current_record)
-    @representation = current_game.representation
-  end
-  
-  def fetch_record(cipher)
-    GameState.find(Cipher.decode_id(cipher))
-  end
-  
-  def reset_two_player_game
-    current_record = fetch_record(params[:cipher].to_i)
-    current_record.reset
-    current_record.save
-    create_player_return(Game.new(current_record.token),current_record)
-  end
-  
-  def two_player_move
-    current_player = Cipher.decode_player(params[:cipher].to_i)
-    current_record = fetch_record(params[:cipher].to_i)
-    current_game = Game.new(current_record.token)
-    
-    if ( current_player == current_record.player ) and
-       ( current_game.can_still_make_moves? ) and
-       ( current_game.available?(params[:move]))
-       
-        current_game.add_move(current_player,params[:move])
-        current_record.token = current_game.memento
-        current_record.switch_player!
-        current_record.save
-    end
-    create_player_return(current_game,current_record)
-  end
-  
-  def create_player_return(game,record)
-    render json: { notification: create_notification(game,record),
-                   representation: game.representation }.to_json
-  end
-    
-  def create_notification(game,record)
-    return game.notification unless game.can_still_make_moves?
+  def create_notification(record)
+    return record.game.notification unless record.game.can_still_make_moves?
     return "Waiting for X move" if "X" == record.player
     return "Waiting for O move" if "O" == record.player
   end
   
-  def get_update
-    current_record = fetch_record(params[:cipher].to_i)
-    current_game = Game.new(current_record.token)
-    create_player_return(current_game,current_record)
-  end
-  
-  def invite
-    new_cipher = Cipher.switch_cipher_player(params[:cipher].to_i)
-    Invite.tic_tac_toe(params[:user_email],
-    "#{request.protocol}#{request.host_with_port}\/#{new_cipher}").deliver
-    redirect_to :back
-  end
 end
--- app/models/game_state.rb
+++ app/models/game_state.rb
@@ -1,14 +1,30 @@
 require "constants"
 
 class GameState < ActiveRecord::Base
-  attr_accessible :token, :player
+  attr_accessor :game, :cipher_player
+
+  def self.create_game
+    current_record = GameState.new
+    current_record.initialize_player
+    current_record.reset
+    current_record.save
+    current_record
+  end
+
+  def self.find_by_cipher(cipher)
+    current_record = GameState.find(Cipher.decode_id(cipher))
+    current_record.game = Game.new(current_record.token)
+    current_record.cipher_player = Cipher.decode_player(cipher)
+    current_record
+  end
   
   def initialize_player
     self.player = ( ( 0 == rand(2) ) ? X_TOKEN : O_TOKEN )
   end
   
   def reset
-    self.token = INITIAL_MEMENTO
+    @game = Game.new(INITIAL_MEMENTO)
+    self.token = @game.memento
     self
   end
   
@@ -25,4 +41,23 @@ class GameState < ActiveRecord::Base
     return self.id * X_FACTOR if ( X_TOKEN == player )
     return self.id * O_FACTOR if ( O_TOKEN == player )
   end
+  
+  def move_if_possible!(move)
+    if possible_to_move?(move)
+      @game.add_move(@cipher_player,move)
+      self.token = @game.memento
+      self.switch_player!
+      self.save
+    end
+  end
+  
+  private
+  
+  def possible_to_move?(move)
+    ( @cipher_player == self.player ) and
+    ( @game.can_still_make_moves? ) and
+    ( @game.available?(move))
+  end
+  
+  
 end
