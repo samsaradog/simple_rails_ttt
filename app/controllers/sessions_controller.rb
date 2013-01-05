5358dd0ca028aef9b17875f1b03463d76e96422b
Shinkyo Will Warner <gwmf7rru35@snkmail.com>
2013-01-05 12:52:19 -0500

    change layouts per customer request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     player = Player.find_by_email(params[:session][:email].downcase)
     if player && player.authenticate(params[:session][:password])
       sign_in player
-      redirect_to player
+      redirect_to root_path
     else
-      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end  
   end
